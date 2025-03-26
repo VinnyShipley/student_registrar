6eaa46ef1442c39a8a758724e5ceee6ddcb9e7f4
Vinny <vinnyshipley@gmail.com>
2025-03-26 15:11:57 -0700

    Finished for 03/26. StudentProfile parses initial string in eventual input.

--- roster.cpp
+++ roster.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
 #include <string>
+#include "roster.h"
 
 using namespace std;
--- roster.h
+++ roster.h
@@ -1,6 +1,12 @@
 #pragma once
 
-
 using namespace std;
 
 
+class Roster {
+	
+	public:
+		// Create the array of dynamic pointers here
+		// Will hold a collection of StudentProfile objects eventually
+		// Form is created here, objects are going to be created in the roster.cpp file
+};
